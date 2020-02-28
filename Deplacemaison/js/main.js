722b966cc31b56a297b026f6c443f6398c34cd34
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-02-28 15:50:50 +0700

    Update SVG animation

--- Deplacemaison/js/main.js
+++ Deplacemaison/js/main.js
@@ -1,3 +1,5 @@
+let log = console.log;
+// owl carousel
 $(document).ready(function() {
   $(".owl-carousel").owlCarousel();
 });
@@ -53,3 +55,82 @@ $("#product-section .row .owl-carousel").owlCarousel({
     }
   }
 });
+
+// anime js
+
+$(".slide")
+  .mouseenter(() => {
+    anime({
+      targets: ".slide-up",
+      translateY: -10,
+      opacity: 0,
+      easing: "easeOutQuart"
+    });
+    anime({
+      targets: ".slide-down",
+      translateY: -10,
+      opacity: 1,
+      easing: "easeOutQuart"
+    });
+  })
+  .mouseleave(() => {
+    anime({
+      targets: ".slide-up",
+      translateY: 0,
+      opacity: 1,
+      easing: "easeOutQuart"
+    });
+    anime({
+      targets: ".slide-down",
+      translateY: 0,
+      opacity: 0,
+      easing: "easeOutQuart"
+    });
+  });
+
+const svgPath = $("svg path");
+
+const svgText = anime({
+  targets: svgPath[0],
+  loop: true,
+  direction: "alternate",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutSine",
+  duration: 700,
+  delay: (el, i) => {
+    return i * 500;
+  }
+});
+
+$(window).ready(() => {
+  $("#c-group path").css("animation", "write 1s forwards");
+  setTimeout(() => {
+    $("#o-group path").css("animation", "write 1s forwards");
+  }, 250);
+  setTimeout(() => {
+    $("#l1-group path").css("animation", "write 1s forwards");
+  }, 500);
+  setTimeout(() => {
+    $("#l2-group path").css("animation", "write 1s forwards");
+  }, 750);
+  setTimeout(() => {
+    $("#point-group path").css("animation", "write 1s forwards");
+  }, 1000);
+  setTimeout(() => {
+    $("#numb1-group path").css("animation", "write 1s forwards");
+  }, 750);
+  setTimeout(() => {
+    $("#numb9-group path").css("animation", "write 1s forwards");
+  }, 1000);
+  setTimeout(() => {
+    $("#slash-group path").css("animation", "write 1s forwards");
+  }, 1250);
+  setTimeout(() => {
+    $("#numb2-group path").css("animation", "write 1s forwards");
+  }, 1500);
+  setTimeout(() => {
+    $("#num0-group path").css("animation", "write 1s forwards");
+  }, 1750);
+});
+
+//  this.CSS({ animation: "write 1s forwards" });
