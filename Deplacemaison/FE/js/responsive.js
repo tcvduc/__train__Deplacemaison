9f1e0b6142f240c98f7c77b80c9a19108c4655d2
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-03-22 16:28:10 +0700

    Fixed hamberger bugs

--- Deplacemaison/FE/js/responsive.js
+++ Deplacemaison/FE/js/responsive.js
@@ -14,46 +14,69 @@ $(window).resize(() => {
 
 // hamberger on click
 
-function toggleHamMidChange() {
-  const hammid = document.querySelector(".ham-mid");
-  if (hammid.style.transform === "rotate(-45deg)") {
-    hammid.style.transform = "rotate(0deg)  ";
-  } else {
-    hammid.style.transform = "rotate(-45deg)";
-  }
-}
+// function toggleHamMidChange() {
+//   const hammid = document.querySelector(".ham-mid");
+//   if (hammid.style.transform === "rotate(-45deg)") {
+//     hammid.style.transform = "rotate(0deg)  ";
+//   } else {
+//     hammid.style.transform = "rotate(-45deg)";
+//   }
+// }
 
 function toggleHamTopChange() {
+  const hamMobileTop = $(".ham-mb-top");
+  const hamMobileBottom = $(".ham-mb-bottom");
+
+  const navMobile = $(".nav-mobile");
+
   const hamTop = $(".ham-top");
+  const hamBottom = $(".ham-bottom");
   if (hamTop.hasClass("ham-top-default")) {
     hamTop.removeClass("ham-top-default");
     hamTop.addClass("ham-top-change");
+
+    // handler nav mobile
+    navMobile.removeClass("d-none").removeClass("nav-mobile-animation-out");
+    navMobile.addClass("d-block").addClass("nav-mobile-animation-in");
+
+    setTimeout(() => {
+      hamMobileTop.removeClass("ham-top-default");
+      hamMobileTop.addClass("ham-top-change");
+    }, 300);
   } else if (hamTop.hasClass("ham-top-change")) {
     hamTop.removeClass("ham-top-change");
     hamTop.addClass("ham-top-default");
+
+    setTimeout(() => {
+      hamMobileTop.removeClass("ham-top-change");
+      hamMobileTop.addClass("ham-top-default");
+    }, 300);
   }
 }
 
 function toggleHamBottomChange() {
+  const hamMobileBottom = $(".ham-mb-bottom");
+
+  const navMobile = $(".nav-mobile");
+
   const hamBottom = $(".ham-bottom");
+
   if (hamBottom.hasClass("ham-bottom-default")) {
     hamBottom.removeClass("ham-bottom-default");
     hamBottom.addClass("ham-bottom-change");
+
+    setTimeout(() => {
+      hamMobileBottom.removeClass("ham-bottom-default");
+      hamMobileBottom.addClass("ham-bottom-change");
+    }, 300);
   } else if (hamBottom.hasClass("ham-bottom-change")) {
     hamBottom.addClass("ham-bottom-default");
     hamBottom.removeClass("ham-bottom-change");
-  }
-}
 
-function navMobile() {
-  const navMobile = $(".nav-mobile");
-  const hamTop = $(".ham-top");
-  if (hamTop.hasClass("ham-top-change")) {
-    navMobile.removeClass("d-none");
-    navMobile.addClass("d-block").addClass("nav-mobile-animation-in");
-  } else {
-    navMobile.addClass("d-none");
-    navMobile.removeClass("d-block");
+    setTimeout(() => {
+      hamMobileBottom.removeClass("ham-bottom-change");
+      hamMobileBottom.addClass("ham-bottom-default");
+    }, 300);
   }
 }
 
@@ -63,23 +86,39 @@ function hambergerMobileClick() {
 
   const navMobile = $(".nav-mobile");
 
+  const hamTop = $(".ham-top");
+  const hamBottom = $(".ham-bottom");
+
   if (
-    hamMobileTop.hasClass("ham-top-change") ||
-    hamMobileBottom.hasClass("ham-bottom-change")
+    hamMobileBottom.hasClass("ham-bottom-default") &&
+    hamMobileTop.hasClass("ham-top-default")
   ) {
-    hamMobileTop.removeClass("ham-top-change");
-    hamMobileTop.addClass("ham-top-default");
-
-    hamMobileBottom.removeClass("ham-bottom-change");
-    hamMobileBottom.removeClass("ham-bottom-default");
-
-    navMobile.removeClass("nav-mobile-animation-in");
-    navMobile.addClass("nav-mobile-animation-out");
-  } else {
     hamMobileTop.removeClass("ham-top-default");
-    hamMobileTop.addClass("ham-top-change");
-
     hamMobileBottom.removeClass("ham-bottom-default");
+
+    hamMobileTop.addClass("ham-top-change");
     hamMobileBottom.addClass("ham-bottom-change");
+  } else {
+    hamMobileTop.removeClass("ham-top-change");
+    hamMobileBottom.removeClass("ham-bottom-change");
+
+    hamMobileTop.addClass("ham-top-default");
+    hamMobileBottom.addClass("ham-bottom-default");
+
+    hamTop.removeClass("ham-top-change");
+    hamBottom.removeClass("ham-bottom-change");
+
+    hamTop.addClass("ham-top-default");
+    hamBottom.addClass("ham-bottom-default");
+
+    // handler nav mobile
+
+    navMobile.removeClass("d-block").removeClass("nav-mobile-animation-in");
+    navMobile.addClass("nav-mobile-animation-out");
+
+    // fixed nav mobile bugs @_@ - hơi dơ
+    setTimeout(() => {
+      navMobile.addClass("d-none");
+    }, 300);
   }
 }
