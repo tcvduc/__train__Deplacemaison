3dfb90177510df509e6fb497e62256aae2c38132
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-08-26 11:26:45 +0700

    nav bug

--- Deplacemaison/client/js/nav.js
+++ Deplacemaison/client/js/nav.js
@@ -1,5 +1,18 @@
 let log = console.log;
 
-function nav() {}
+function navLeftBottomHoverHandle(e) {
+  this.children[0].classList.add("roll");
+  this.children[1].classList.add("roll");
+}
+
+function nav() {
+  const nav_left_bottom_wrapper = document.querySelectorAll(
+    ".nav-left-bottom  .nav-link-wrapper"
+  );
+
+  nav_left_bottom_wrapper.forEach((ele) => {
+    ele.addEventListener("mouseover", navLeftBottomHoverHandle);
+  });
+}
 
 nav();
